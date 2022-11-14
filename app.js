91f15167b7004bb62a95a3471f8d081da38acdfe
theo anache <theoo.anache@gmail.com>
2022-11-14 23:40:45 +0100

    express04 exo

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.get("/api/users/:id", usersHandlers.getUserById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", usersHandlers.postUser);
 
+app.put("/api/movies/:id",movieHandlers.updateMovie );
+app.put("/api/users/:id", usersHandlers.updateUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- usersHandlers.js
+++ usersHandlers.js
@@ -46,10 +46,33 @@ res.location(`/api/users/${result.insertId}`).sendStatus(201);
     });
 };
 
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+    .query (
+        "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+     .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+    
 
 
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 };
